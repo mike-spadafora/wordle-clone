0998749b3eec18b744f2a586e350e8539a23a620
mike-spad <miikekm@gmail.com>
2022-01-29 19:10:40 -0500

    set up prettier for formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,43 @@
 import { useEffect, useState } from "react";
-import logo from './logo.svg';
-import styled from "styled-components"
-import './App.css';
-import { SlotState } from './SlotState.ts';
-
+import logo from "./logo.svg";
+import styled from "styled-components";
+import "./App.css";
+import { SlotState } from "./SlotState.ts";
 
 function App() {
-  const [wordGrid, setWordGrid] = useState([])
-  const NUM_GUESSES = 6
-  const WORD_SIZE = 5
+  const [wordGrid, setWordGrid] = useState([]);
+  const NUM_GUESSES = 6;
+  const WORD_SIZE = 5;
 
   useEffect(() => {
     function initializeWordGrid() {
-      let newWordGrid = []
+      let newWordGrid = [];
       //create the rows (from guesses)
       for (let i = 0; i < NUM_GUESSES; i++) {
-        newWordGrid.push([])
+        newWordGrid.push([]);
       }
 
       //initialize empty spots
       for (let i = 0; i < NUM_GUESSES; i++) {
         for (let j = 0; j < WORD_SIZE; j++) {
-          newWordGrid[i].push(
-            { letter: "a", state: SlotState.empty }
-          );
+          newWordGrid[i].push({ letter: "a", state: SlotState.empty });
         }
       }
 
-      setWordGrid(newWordGrid)
+      setWordGrid(newWordGrid);
     }
     if (wordGrid.length === 0) {
-      initializeWordGrid()
+      initializeWordGrid();
     }
-  })
+  });
 
   return (
     <Div>
-      {wordGrid.map((row) =>
-      (
+      {wordGrid.map((row) => (
         <RowWrapper>
-          {row.map((col) => <div> {col.letter}</div>)}
+          {row.map((col) => (
+            <div> {col.letter}</div>
+          ))}
         </RowWrapper>
       ))}
     </Div>
@@ -52,11 +50,11 @@ const Div = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 8px;
-`
+`;
 
 const RowWrapper = styled.div`
-display: flex;
-gap:8px;
-`
+  display: flex;
+  gap: 8px;
+`;
 
 export default App;
