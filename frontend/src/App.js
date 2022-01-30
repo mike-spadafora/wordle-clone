aa047c2aafeaa72c28dedbcdb0f5f0016425aaa4
mike-spad <miikekm@gmail.com>
2022-01-29 19:01:28 -0500

    base infra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
+import styled from "styled-components"
 import './App.css';
+import { SlotState } from './SlotState.ts';
+
 
 function App() {
+  const [wordGrid, setWordGrid] = useState([])
+  const NUM_GUESSES = 6
+  const WORD_SIZE = 5
+
+  useEffect(() => {
+    function initializeWordGrid() {
+      let newWordGrid = []
+      //create the rows (from guesses)
+      for (let i = 0; i < NUM_GUESSES; i++) {
+        newWordGrid.push([])
+      }
+
+      //initialize empty spots
+      for (let i = 0; i < NUM_GUESSES; i++) {
+        for (let j = 0; j < WORD_SIZE; j++) {
+          newWordGrid[i].push(
+            { letter: "a", state: SlotState.empty }
+          );
+        }
+      }
+
+      setWordGrid(newWordGrid)
+    }
+    if (wordGrid.length === 0) {
+      initializeWordGrid()
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      {wordGrid.map((row) =>
+      (
+        <RowWrapper>
+          {row.map((col) => <div> {col.letter}</div>)}
+        </RowWrapper>
+      ))}
+    </Div>
   );
 }
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`
+
+const RowWrapper = styled.div`
+display: flex;
+gap:8px;
+`
 
 export default App;
